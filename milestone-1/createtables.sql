e7aca122cbf484c84994a382175b7c6ab4adb71b
Musab Mufti <mfmufti@outlook.com>
2025-10-20 17:03:33 -0400

    updated tables some

--- milestone-1/createtables.sql
+++ milestone-1/createtables.sql
@@ -1,9 +1,16 @@
 CREATE TABLE User
 (uid INT NOT NULL PRIMARY KEY,
 username VARCHAR(30) NOT NULL CHECK(LENGTH(username) > 0),
-password VARCHAR(30) NOT NULL,
-name VARCHAR(30) NOT NULL,
-role VARCHAR(10) NOT NULL
+password VARCHAR(30) NOT NULL CHECK(LENGTH(password) >= 8),
+name VARCHAR(30) NOT NULL CHECK,
+);
+
+CREATE TABLE Admin
+(uid INT NOT NULL PRIMARY KEY REFERENCES User(uid)
+);
+
+CREATE TABLE Adopter
+(uid INT NOT NULL PRIMARY KEY REFERENCES User(uid)
 );
 
 CREATE TABLE Pokemon
@@ -13,28 +20,29 @@ type1 VARCHAR(30) NOT NULL,
 type2 VARCHAR(30),
 height FLOAT(4),
 weight FLOAT(4),
-abilities VARCHAR(250) NOT NULL,
+generation INTEGER NOT NULL,
+is_legendary BOOLEAN NOT NULL,
+abilities VARCHAR(100) NOT NULL,
 base_hapiness INTEGER NOT NULL,
 hp INTEGER NOT NULL,
 attack INTEGER NOT NULL,
 defense INTEGER NOT NULL,
 speed INTEGER NOT NULL,
-generation INTEGER NOT NULL,
-is_legendary BOOLEAN NOT NULL,
 );
 
 CREATE TABLE AdoptablePokemon
-(adopt_id INT NOT NULL PRIMARY KEY,
+(pid INT NOT NULL PRIMARY KEY,
 pokedex_number INT NOT NULL REFERENCES Pokemon(pokedex_number),
 nickname VARCHAR(30) NOT NULL,
-add_date DATE NOT NULL,
+date_added DATE NOT NULL,
+description VARCHAR(100) NOT NULL,
+status ENUM('available', 'taken') NOT NULL,
 );
 
-CREATE TABLE AdoptRecord
-(pokedex_number INT NOT NULL REFERENCES Pokemon(pokedex_number),
+CREATE TABLE AdoptionLogs
+(log_id INT NOT NULL AUTO_INCREMENT
+pokedex_number INT NOT NULL REFERENCES Pokemon(pokedex_number),
 adopt_id INT NOT NULL REFERENCES AdoptablePokemon(adopt_id),
-old_nickname VARCHAR(30) NOT NULL,
-new_nickname VARCHAR(30) NOT NULL,
 adopt_date DATE NOT NULL,
-PRIMARY KEY(pokedex_number, adopt_id)
+action_type ENUM('adopt', 'unadopt'),
 );
