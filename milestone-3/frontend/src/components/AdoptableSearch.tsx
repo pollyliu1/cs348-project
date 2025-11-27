2c43e63a8b9d8de22808cd8789283e93b0f66092
Stanley Lin <stanleylin82@gmail.com>
2025-11-26 19:12:18 -0500

    added toggle for only my pokemon

--- milestone-3/frontend/src/components/AdoptableSearch.tsx
+++ milestone-3/frontend/src/components/AdoptableSearch.tsx
@@ -12,29 +12,37 @@ export default function AdoptableSearch({ onResults, onStart }: Props) {
   const [q, setQ] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [relevanceSelected, setRelevanceSelected] = useState<boolean>(false);
+  const [onlyMyPokemonSelected, setOnlyMyPokemonSelected] =
+    useState<boolean>(false);
+
   const { userId } = useAuth();
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
-      const url = new URL("/api/search-adoptable-pokemon", window.location.origin);
+      const url = new URL(
+        "/api/search-adoptable-pokemon",
+        window.location.origin
+      );
       url.searchParams.set("name", q);
       url.searchParams.set("uid", userId || "");
-      url.searchParams.set("order", relevanceSelected ? "relevance" : "compatibility");
-			const response = await fetch(url);
+      url.searchParams.set(
+        "order",
+        relevanceSelected ? "relevance" : "compatibility"
+      );
+      const response = await fetch(url);
 
-			if (!response.ok) {
-				console.log("error is: ", await response.text());
-			}
+      if (!response.ok) {
+        console.log("error is: ", await response.text());
+      }
 
       const x = await response.json();
-      console.log("results:", x)
+      console.log("results:", x);
       onStart?.();
       onResults?.(x);
-
-		} catch (err: any) {
-			console.log(err.toString());
-		}
+    } catch (err: any) {
+      console.log(err.toString());
+    }
   }
 
   return (
@@ -56,7 +64,7 @@ export default function AdoptableSearch({ onResults, onStart }: Props) {
           {loading ? "Searching…" : "Search"}
         </button>
       </form>
-      <div className="mb-8 mt-2 ml-2">
+      <div className="mb-8 mt-4 ml-2 flex gap-4">
         <Switch.Root
           checked={relevanceSelected}
           onCheckedChange={(e) => setRelevanceSelected(e.checked)}
@@ -66,6 +74,16 @@ export default function AdoptableSearch({ onResults, onStart }: Props) {
             <Switch.Thumb />
           </Switch.Control>
           <Switch.Label>Sort by Relevance</Switch.Label>
+        </Switch.Root>
+        <Switch.Root
+          checked={onlyMyPokemonSelected}
+          onCheckedChange={(e) => setOnlyMyPokemonSelected(e.checked)}
+        >
+          <Switch.HiddenInput />
+          <Switch.Control className="ring-1 ring-white text-white">
+            <Switch.Thumb />
+          </Switch.Control>
+          <Switch.Label>Only My Pokémon</Switch.Label>
         </Switch.Root>
       </div>
     </div>
